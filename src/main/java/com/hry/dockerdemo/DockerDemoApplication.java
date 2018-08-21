eff3e360b1de7aeba7ff884679d0319e72c2975e
unknown <tianpy5210@gmail.com>
2018-08-21 14:26:18 +0800

    first commit

---
+++ src/main/java/com/hry/dockerdemo/DockerDemoApplication.java
@@ -0,0 +1,21 @@
+package com.hry.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class DockerDemoApplication {
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello(){
+		return "hello";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+}
